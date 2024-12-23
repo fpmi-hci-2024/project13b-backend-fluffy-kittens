096c77a21bcdb13bb2c37fecca2454bf685521fb
Tsimafei Petrykevich <petrik3003@gmail.com>
2024-12-23 15:01:17 +0300

    Modify CORS middleware to allow post/put/delete requests

--- internal/utils/cors.go
+++ internal/utils/cors.go
@@ -13,6 +13,15 @@ func EnableCORS(next http.Handler) http.Handler {
 		// Allow requests from specific origins
 		if origin == "https://project13b-web-fluffy-kittens.onrender.com" || origin == "http://localhost:4200" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		// Handle preflight OPTIONS requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
 		}
 
 		// Call the next handler in the chain
